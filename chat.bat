c5a29d6e9377055216a151327a34971740bcada4
Hid2enTech <systemdeveloper868@gmail.com>
2020-05-01 12:49:41 +0200

    renombremos fichero

--- chat.bat
+++ chat.bat
@@ -1,12 +1,14 @@
 @echo off
 @ cls
 echo "**Mensajeria P2p Cifrada**"
+echo .
 echo Levanta: ipfs daemon 
 
 :inicio
+
 set /p texto="Usted:> " 
-ipfsmsg.exe -e --llave hola --txt %texto%
+msg.exe -e --llave hola --txt %texto%
 set /p cont="secuencia:> "
 ipfs name publish %cont%
-ipfsmsg.exe -d --llave hola --txt .
+msg.exe -d --llave hola --txt .
 goto inicio
